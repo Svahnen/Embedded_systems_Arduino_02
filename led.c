6f14d92542932c6302c37883ca65290f5aa27385
Svahnen <svahn.jonny@gmail.com>
2022-03-14 11:37:13 +0100

    Set LED pins to inputs

--- led.c
+++ led.c
@@ -3,13 +3,9 @@
 #include <avr/io.h>
 
 void LED_init() {
-    DDRB |= (1 << BLUE);  // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 1, this makes PB1 an output
-    DDRB |= (1 << GREEN);
-    DDRB |= (1 << RED);
-
-    PORTB |= (1 << BLUE);  // Set PB1 in PORTB to initial 1 (high) so it is off
-    PORTB |= (1 << GREEN);
-    PORTB |= (1 << RED);
+    DDRB &= ~(1 << BLUE);  // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 0, this makes PB1 an input so we can jump to a interupt instead
+    DDRB &= ~(1 << GREEN);
+    DDRB &= ~(1 << RED);
 }
 
 void LED_toggle(int LED) {
--- timer.c
+++ timer.c
@@ -7,6 +7,4 @@ void timer_init() {
     TCCR0B = (1 << CS10) | (1 << CS12);  // Timer mode with 1024 prescaler
     TCCR0A &= ~(1 << WGM00);             // CTC mode
     TCCR0A |= (1 << WGM01);              // CTC mode
-
-    // TIMSK1 |= (1 << OCIE0A);
 }
