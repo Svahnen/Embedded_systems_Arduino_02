2531e0d75d6077dbefeef79f80613e8ebda31ebb
Svahnen <svahn.jonny@gmail.com>
2022-03-14 13:28:57 +0100

    Set PWM mode

--- main.c
+++ main.c
@@ -12,30 +12,12 @@ int main(void) {
     uart_init();
     LED_init();
 
-    LED_toggle(RED);
-    int time1 = 0;
-    int time2 = 0;
-    int time3 = 0;
-
     while (1) {
-        // OCF0B | (1<< TIFR0))
-        // Instead of bit_is_set it should be possible to shift the bit all the way to the right and see if its == 1
-        if (bit_is_set(TIFR0, OCF0B)) {  // Look at overflow bit, 16 000 000 / 1024
-            time1++;
-            time2++;
-            TIFR0 |= (1 << OCF0B);  // Reset bit
-        }
-        if (time1 == 15625) {  // Count seconds, 16 000 000 Hz / 1024 = 15625 Hz
-            // LED_toggle(RED);
-            time1 = 0;
-        }
-        if (time2 == 156) {  // Count ~10ms, 1 % of 15625 Hz ~156 Hz (1000ms for one second, 10ms = 1% of 1000).
-            time3++;
-            time2 = 0;
-        }
-        if (time3 == 10) {  // Count ~100ms, ~10ms * 10 = ~100ms
-            LED_toggle(RED);
-            time3 = 0;
-        }
+        OCR0A = 255;
+        _delay_ms(1000);
+        OCR0A = 100;
+        _delay_ms(1000);
+        OCR0A = 0;
+        _delay_ms(1000);
     }
 }
--- timer.c
+++ timer.c
@@ -5,6 +5,13 @@
 void timer_init() {
     TCNT0 = 0;                           // Clear the counter
     TCCR0B = (1 << CS10) | (1 << CS12);  // Timer mode with 1024 prescaler
-    TCCR0A &= ~(1 << WGM00);             // CTC mode
-    TCCR0A |= (1 << WGM01);              // CTC mode
+
+    // TOP is defined as 0xFF when WGM0[2:0]=0x3 (page 132 in datasheet)
+    TCCR0A |= (1 << WGM00);
+    TCCR0A |= (1 << WGM01);
+    TCCR0A &= ~(1 << WGM02);
+
+    // Writing the TCCR0A.COM0x [1:0] bits to 0x2 will produce a non - inverted PWM (page 133 in datasheet)
+    TCCR0A &= ~(1 << COM0A0);
+    TCCR0A |= (1 << COM0A0);
 }
