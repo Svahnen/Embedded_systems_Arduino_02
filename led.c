75628cb3e6addcc6dd767f3df922fb12a97e08b8
Svahnen <svahn.jonny@gmail.com>
2022-03-14 12:50:53 +0100

    PD6

--- led.c
+++ led.c
@@ -6,6 +6,8 @@ void LED_init() {
     DDRB &= ~(1 << BLUE);  // Set PB1 (Pin9, blue LED on the shield) in DDRB (Port B Data Direction Register) to 0, this makes PB1 an input so we can jump to a interupt instead
     DDRB &= ~(1 << GREEN);
     DDRB &= ~(1 << RED);
+    // OC0A_PIN
+    DDRB |= (1 << PD6);  // Set OC0A to output
 }
 
 void LED_toggle(int LED) {
