5ddd79038d2aca2bdd4141d8e5dfc20c04519d8f
Svahnen <svahn.jonny@gmail.com>
2022-03-15 10:09:58 +0100

    Update WGM02 register

--- timer.c
+++ timer.c
@@ -14,7 +14,7 @@ void timer_init() {
     // TOP is defined as 0xFF when WGM0[2:0]=0x3 (page 132 in datasheet)
     TCCR0A |= (1 << WGM00);
     TCCR0A |= (1 << WGM01);
-    TCCR0A &= ~(1 << WGM02);
+    TCCR0B &= ~(1 << WGM02);
 
     // Writing the TCCR0A.COM0x [1:0] bits to 0x2 will produce a non - inverted PWM (page 133 in datasheet)
     TCCR0A &= ~(1 << COM0A0);
